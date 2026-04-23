5b07c2f41e490b1d40501ff5a100379976bf99cb
sima00rnmr <sima92rnmr@gmail.com>
2026-04-23 19:18:02 +0900

    GoogleマップのURLを作成

--- moattravel_3s/src/main/java/com/example/moattravel/controller/ShopController.java
+++ moattravel_3s/src/main/java/com/example/moattravel/controller/ShopController.java
@@ -1,5 +1,7 @@
 package com.example.moattravel.controller;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.moattravel.entity.Shop;
 import com.example.moattravel.entity.User;
@@ -62,4 +65,24 @@ public class ShopController {
 
 		return "shops/category_list";
 	}
+	
+	//詳細ページからGoogleマップへのリンクをクリックした場合の動き
+	@GetMapping("/shops/{id}/map")
+	public RedirectView openMap(@PathVariable Integer id,
+	        @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+
+	    Shop shop = shopRepository.findById(id).orElseThrow();
+
+	    if (userDetailsImpl != null) {
+	        User user = userDetailsImpl.getUser();
+	        userActionService.saveAction(user, shop, "map");
+	    }
+
+	    String query = shop.getName() + " " + shop.getAddress();
+	    String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
+	    String url = "https://www.google.com/maps/search/?api=1&query=" + encodedQuery;
+
+	    return new RedirectView(url);
+	}
+
 }
